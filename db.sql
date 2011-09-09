a378b2a841466bf0701a1819c2729fe58a59c753
Rafał Filipek <rafal.filipek@gmail.com>
2011-09-09 23:12:52 +0200

    Update sql dump

--- db.sql
+++ db.sql
@@ -4,3 +4,15 @@ CREATE TABLE `Category` (
    `marked` tinyint(1) default '0',
    PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `Bookmark` (
+   `id` int(11) not null auto_increment,
+   `href` varchar(255) not null,
+   PRIMARY KEY (`id`)
+)
+
+CREATE TABLE `bookmark_category` (
+   `bookmark_id` int(11) not null,
+   `category_id` int(11) not null,
+   KEY `bookmark_id` (`bookmark_id`,`category_id`)
+);
